0cb4d99b5599c64337c6bd6b56ee18fc9bf5d482
AdityaJandu <janduaditay@gmail.com>
2025-10-27 19:00:46 +0530

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from "dotenv";
+import path from "path";
 
 import rateLimiter from "./middlewares/rateLimiter.js";
 import notesRoutes from "./routes/notes_route.js";
@@ -11,12 +12,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const __dirName = path.resolve();
+
 // MIDDLEWARE: Parse the JSON body like => req.body -> then can get the required fields..
-app.use(cors(
-    {
-        origin: "http://localhost:5173",
-    }
-));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors(
+        {
+            origin: "http://localhost:5173",
+        }
+    ));
+}
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -29,6 +34,13 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirName, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirName, "../frontend", "dist", "index.html"));
+    });
+}
 
 
 connectDB().then(() => {
